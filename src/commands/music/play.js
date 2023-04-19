e624510b57f0673f498b478837c7c644fa3a6cc1
Humildeficador <joao.oliveira1270@etec.sp.gov.br>
2023-04-19 14:15:22 -0300

    play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,5 +1,6 @@
 const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const { QueryType } = require('discord-player');
+const capitalize = require('../../utils/capitalize')
 
 module.exports = {
     name: 'play',
@@ -9,53 +10,79 @@ module.exports = {
     //deleted: true,
     options: [
         {
-            name: 'url',
-            description: 'Busca a URL da música no YT.',
+            name: 'termo',
+            description: 'Busca uma música.',
             required: true,
             type: ApplicationCommandOptionType.String,
         },
     ],
-
     callback: async (client, interaction) => {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({
-                content: 'Você precisa está em um canal de voz para usar esse comando!',
-                ephemeral: false,
+        try {
+            /* Verifica se o membro está em um canal */
+            if (!interaction.member.voice.channel) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user} você precisa está em um canal de voz para usar esse comando.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed],
+                    ephemeral: true,
+                });
+                return;
+            }
+            /* Faz a busca do termo */
+            let termo = interaction.options.getString('termo');
+            const result = await client.player.search(termo, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO,
             });
-            return;
+
+            /* Verifica se tem resultado */
+            if (!result.hasTracks()) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user} nenhum resultado encontrado.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed],
+                    ephemeral: false,
+                });
+                return;
+            }
+
+            const track = result.tracks[0];
+            const source = capitalize(track.raw.source)
+
+            /* Cria o Embed */
+            const embed = new EmbedBuilder()
+                .setAuthor({ name: source })
+                .setTitle(`${track.title}`)
+                .setImage(track.thumbnail)
+                .setURL(track.url)
+                .setColor('Random')
+                .setDescription(`Views: ${track.views}`)
+                .setFooter({ text: `Duração: ${track.duration}` })
+            await interaction.reply({ embeds: [embed] });
+
+            const channel = interaction.member.voice.channel;
+            /* Tudo OK */
+            await client.player.play(channel, track, {
+                nodeOptions: {
+                    metadata: interaction,
+                    leaveOnEmpty: null,
+                    leaveOnEmptyCooldown: 300000,
+                    leaveOnEnd: null,
+                    leaveOnEndCooldown: 300000,
+                },
+            });
+
+        } catch (error) {
+            console.log(`Ocorreu um erro ${error}`);
+            const embed = new EmbedBuilder()
+                .setDescription(`${interaction.user} Tente novamente em alguns segundos...`)
+                .setColor('Random');
+            await interaction.reply({
+                content: { embeds: [embed] },
+                ephemeral: true,
+            });
         }
-        await interaction.deferReply();
-
-        /* Faz a busca da URL */
-        let url = interaction.options.getString('url');
-        const result = await client.player.search(url, {
-            requestedBy: interaction.user,
-            searchEngine: QueryType.YOUTUBE_VIDEO,
-        });
-
-        /* Verifica se tem resultado */
-        if (!result.hasTracks()) {
-            await interaction.editReply("Nenhum resultado encontrado.");
-            return;
-        }
-
-        const tracks = result.tracks[0];
-        /* Cria o Embed */
-        const embed = new EmbedBuilder()
-            .setTitle(`${tracks.title}`)
-            .setImage(tracks.thumbnail)
-            .setURL(tracks.url)
-            .setColor('Random')
-            .setDescription(`Views: ${tracks.views}`)
-            .setFooter({text: `Duração: ${tracks.duration}`})
-        await interaction.editReply({ embeds: [embed] })
-
-        await client.player.play(interaction.member.voice.channel, tracks, {
-            nodeOptions: {
-                metadata: interaction,
-            },
-            leaveOnEnd: false,
-            leaveOnEndCooldown: 300,
-        });
     },
 };
