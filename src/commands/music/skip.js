38a629fd08a624c47c7b53fcd6bab6752ea360d2
Humildeficador <joao.oliveira1270@etec.sp.gov.br>
2023-04-19 14:19:18 -0300

    skip.js

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -7,21 +7,94 @@ module.exports = {
     //devOnly: Boolean,
     //testOnly: Boolean,
     //deleted: true,
-
     callback: async (client, interaction) => {
+        try {
+            const channelBot = interaction.guild.members.me.voice.channel;
+            const channelMember = interaction.member.voice.channel;
+            /* Verifica se o membro está em um canal */
+            if (!channelMember) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user}, você precisa está em um canal de voz para usar esse comando.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed],
+                    ephemeral: true,
+                });
+                return;
+            };
 
-        await interaction.deferReply();
+            /* Membro no mesmo canal do bot / Bot não está em um canal */
 
-        const queue = useQueue(interaction.guild);
-        if (!queue) return interaction.editReply('Nenhuma música tocando no momento');
-        const tracks = queue.tracks.toArray();
+            if (!channelBot) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user}, não estou em um canal de voz.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed], 
+                    ephemeral: true,
+                });
+                return;
+            };
 
-        const embed = new EmbedBuilder()
-            .setDescription(`${interaction.user} pulou uma música.\n\n \`Proxima música:\`\n **${tracks[0].duration} - ${tracks[0].title} - ${tracks[0].requestedBy}`)
-            .setThumbnail(tracks[0].thumbnail)
-            .setColor('Random');
+            if (channelBot != channelMember) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user}, você precisa está no mesmo canal de voz que eu para usar esse comando.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed], 
+                    ephemeral: true,
+                });
+                return;
+            };
 
-        await interaction.editReply({ embeds: [embed] });
-        queue.node.skip();
+            /* Cria a fila e a pega a música atual */
+            const queue = useQueue(interaction.guild);
+            const tracks = queue.tracks.toArray();
+            const currentTrack = queue.currentTrack;
+
+            /* Nenhuma música tocando */
+            if (queue.node.isIdle()) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user}, não estou tocando nenhuma música no momento.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed], 
+                    ephemeral: true,
+                });
+                return;
+            };
+
+            /* Nenhuma música na fila */
+            if (!tracks[0]) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user} pulou uma música.\n\n${currentTrack.title} - ${currentTrack.requestedBy}`)
+                    .setThumbnail(currentTrack.thumbnail)
+                    .setColor('Random');
+                await interaction.reply({ embeds: [embed] });
+                queue.node.skip();
+                return;
+            };
+
+            /* Tudo OK */
+            const embed = new EmbedBuilder()
+                .setDescription(`${interaction.user} pulou uma música.\n\n **\`Proxima música:\`**\n[${tracks[0].duration}] - ${tracks[0].title} - ${tracks[0].requestedBy}`)
+                .setThumbnail(tracks[0].thumbnail)
+                .setColor('Random');
+
+            await interaction.reply({ embeds: [embed] });
+            queue.node.skip();
+
+        } catch (error) {
+            console.log(`Ocorreu um erro: ${error}`);
+            const embed = new EmbedBuilder()
+                .setDescription(`${interaction.user} Tente novamente em alguns segundos...`)
+                .setColor('Random');
+
+            await interaction.reply({
+                content: { embeds: [embed] },
+                ephemeral: true,
+            });
+            return;
+        };
     },
 };
