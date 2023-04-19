62357f94fe7888a0057ba6b1464a13b8dadea629
Humildeficador <joao.oliveira1270@etec.sp.gov.br>
2023-04-19 14:18:45 -0300

    add remove.js

---
+++ src/commands/music/remove.js
@@ -0,0 +1,96 @@
+const { useQueue } = require('discord-player');
+const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'remove',
+    description: 'Remove uma música da fila!',
+    //devOnly: Boolean,
+    //testOnly: Boolean,
+    //deleted: true,
+    options: [
+        {
+            name: 'musica',
+            description: 'Remove a música equivalente na fila.',
+            required: true,
+            type: ApplicationCommandOptionType.String,
+        },
+    ],
+    callback: async (client, interaction) => {
+        try {
+            const channelBot = interaction.guild.members.me.voice.channel;
+            const channelMember = interaction.member.voice.channel;
+            /* Verifica se o membro está em um canal */
+            if (!channelMember) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user} você precisa está em um canal de voz para usar esse comando.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed], 
+                    ephemeral: true,
+                });
+                return;
+            };
+
+            /* Membro no mesmo canal do bot / Bot não está em um canal */
+            
+            if (!channelBot) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user} não estou tocando nenhuma música.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed], 
+                    ephemeral: true,
+                });
+                return;
+            };
+
+            if (channelBot != channelMember) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`${interaction.user} você precisa está no mesmo canal de voz que eu para usar esse comando.`)
+                    .setColor('Random');
+                await interaction.reply({
+                    embeds: [embed], 
+                    ephemeral: true,
+                });
+                return;
+            };
+
+            const queue = useQueue(interaction.guild.id);
+            const tracks = queue.tracks.toArray();
+            const currentTrack = queue.currentTrack;
+
+            /* Nenhuma música na fila */
+            if (!tracks[0]) {
+                const embed = new EmbedBuilder()
+                    .setDescription(`**\`Musica Atual:\n\`**${currentTrack.title} - ${currentTrack.requestedBy}\n Nenhuma música para ser removida da fila.`)
+                    .setThumbnail(currentTrack.thumbnail)
+
+                await interaction.reply({ embeds: [embed] });
+                return;
+            };
+
+            let musica = (interaction.options.getString('musica') - 1);
+            const embed = new EmbedBuilder()
+                .setDescription(`**\`Música removida\`**\n
+                ${interaction.user} removeu:\n
+                **\`${musica + 1}) [${tracks[musica].duration}]\`**${tracks[musica].title} - ${tracks[musica].requestedBy}`)
+                .setThumbnail(tracks[musica].thumbnail)
+                .setColor('Random');
+            await interaction.reply({ embeds: [embed] });
+            queue.removeTrack(musica);
+
+
+        } catch (error) {
+            console.log(`Ocorreu um erro ${error}`);
+            const embed = new EmbedBuilder()
+                .setDescription(`${interaction.user} Tente novamente em alguns segundos...`)
+                .setColor('Random');
+            await interaction.reply({
+                content: { embeds: [embed] },
+                ephemeral: true,
+            });
+        }
+
+
+    },
+}
